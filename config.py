dbbf82e869ae8fba7c192b6bd64044560bb059b1
Shane Walsh <swalsh818@gmail.com>
2025-03-02 17:20:30 +0000

    Minor line edits

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
-
 MONGODB_URI = os.environ.get('MONGODB_URI')
 
-if not MONGODB_URI:
+if not MONGODB_URI: # If MONGODB_URI is not set in the environment
     raise ValueError("Missing required environment variables. Please check your .env file")
